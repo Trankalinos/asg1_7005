0f65f554589153b5e1a0451c184925a51fa276cc
Trankalinos <tran.sax-maestro@hotmail.com>
2013-09-30 20:36:33 -0700

    Final Changes for Java.

--- JavaClient/src/JavaClient/JavaClient.java
+++ JavaClient/src/JavaClient/JavaClient.java
@@ -15,18 +15,15 @@ package JavaClient;
  * 
  * Usage of JavaClient:
  * 
- * java JavaClient [address] [port] [GET | SEND] [filePath/fileName]
- *              [address]   = the ip address of the server
- *              [port]      = the port specified by the server
- *              [GET]       = as the client, to retrieve from the server
- *              [SEND]      = as the client, to send to the server
- *    [[filePath]/fileName] = specifying the file to be sent or retrieved
- *                            The filePath is not required if the method is GET.    
+ * java -jar "JavaClient.jar"
+ *      Enter Server IP Address: [Server IP address]
+ *      Enter Server Port:       [Server Port]
+ *      Enter Method:            [GET | SEND]
+ *      Enter File Name:         [file name]
  * 
  * @author  David Tran      Java
  * @partner Martin Javier   Ruby
  */
-
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.File;
@@ -36,72 +33,104 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class JavaClient {
 
-    // public JavaClient() {}
-    /**
-     * @param args the command line arguments
-     */
-    static private final int BUFFER = 2048;
-    static private int count;
-    static private Socket sock;
-    static private String address = "";
-    static private int port;
-    static private String method = "";
-    static private String fileName = "";
+    static private final int BUFFER = 8196;         // An int to specify buffer for transfer.
+    static private int count;                       // A global variable placeholder for int.
+    static private Socket sock;                     // Globalized socket object
+    static private String address = "127.0.01";     // Server address; default value: 127.0.0.1
+    static private int port = 7005;                 // Server port; default value: 7005
+    static private String method = "GET";           // Method; default value: GET
+    static private String fileName = "text.txt";    // FileName; default value: text.txt
+    static private OutputStream sout = null;        // Globalized output stream object
 
+    /**
+     * User enters parameters in main method. 
+     * Application then attempts to connect to server. If it does...
+     * then the user specified directions will take place.
+     * 
+     */
     public static void main(String[] args) {
-        address = "127.0.0.1";
-        port = 7005;
-        method = "GET"; // cmd line argument 1
-        fileName = "text.txt"; // cmd line argument 2    
+        
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter Server IP Address: ");
+        address = scan.next();
+        System.out.println();
+        System.out.print("Enter Server Port: ");
+        port = scan.nextInt();
+        System.out.println();
+        System.out.print("Enter Method: ");
+        method = scan.next();
+        System.out.println();
+        System.out.print("Enter File Name: ");
+        fileName = scan.next();
+        
         try {
+            // Connect to the Server.
             sock = new Socket(address, port);
 
             System.out.println("Connection Established: " + sock.getInetAddress()
                     + " port: " + sock.getPort());
 
+            // Prep the sockets for input / output exchange.
             InputStream is = sock.getInputStream();
             System.out.println("Listening...");
 
-            OutputStream sout = null;
             try {
                 sout = sock.getOutputStream();
             } catch (IOException ioe) {
-                System.err.println("Something fucked up.");
+                System.err.println("Encountered an error.");
+                ioe.printStackTrace();
             }
-
             
-
-            //method = "SEND";
-            //fileName = "Flight.docx";
-
+            // Send a packet to the Server with our method and output specs.
             sendPacket(method, sout);
             System.out.println("Sending packet...");
 
             // listen for ACK from Server, then send request for file.
-            listen(is, method, fileName);
-
+            listen(is);
+            
+            // close the socket after transfer is finished.
             sock.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    static public void sendPacket(String str, OutputStream sout) {
+    /**
+     * sendPacket() is an internal method that the application uses to send 
+     * messages to the Server.
+     * 
+     * The server has written protocols that will either accept or reject actions
+     * based on the validity of the message.
+     * 
+     * @param str - the Message to send to the Server.
+     * @param sout - the Output Stream.
+     */
+    static private void sendPacket(String str, OutputStream sout) {
         byte[] buf = str.getBytes();
 
         try {
+            // Send the message
             sout.write(buf);
             System.out.println("Sending request: '" + str + "' ...");
         } catch (IOException ioe) {
-            System.err.println("Packet sending fucked up.");
+            System.err.println("Packet sending messed up. Terminating connection.");
             return;
         }
     }
 
-    static public void listen(InputStream istr, String method, String fileName) {
+    /**
+     * listen() is an internal method that the application uses to listen from the
+     * Server. It has a a set of protocols that, upon receiving the expected message
+     * from the Server, either requests a file from the Server or to upload to the
+     * Server.
+     * 
+     * @param istr - the Input Stream of the application listening from the Server
+     */
+    static private void listen(InputStream istr) {
         byte[] buf = new byte[BUFFER];
         int len = 0;
         while (true) {
@@ -113,24 +142,28 @@ public class JavaClient {
                 break;	// probably a socket ABORT; treat as a close
             } catch (Exception e) {
                 e.printStackTrace();
-            } 
+            }
             if (len == -1) {
                 break;
             }
-            
+
+            // Listen for packets.
             String str = new String(buf, 0, len);
             System.out.println("Received: '" + str + "' from Host.");
 
-            if (str.toString().equalsIgnoreCase("ACK")) {
+            if (str.toString().trim().equalsIgnoreCase("ACK")) {
                 if (method.toString().equalsIgnoreCase("GET")) {
-                    // sendPacket('text.txt', );
+                    // Send a packet with our desired filename to be retrieved 
+                    // from the server.
+                    sendPacket(fileName, sout);
+
                     System.out.println("Calling sendPacket() Method.");
-                    System.out.println("Requesting file: 'text.txt'.");
+                    System.out.println("Requesting file: " + fileName + "...");
                     try {
                         FileOutputStream fos = new FileOutputStream(fileName);
                         BufferedOutputStream bos = new BufferedOutputStream(fos);
 
-                        while ((count = istr.read(buf)) >= 0) {
+                        while ((count = istr.read(buf)) > -1) {
                             bos.write(buf, 0, count);
                         }
                         System.out.println("File Transfer finished.");
@@ -141,24 +174,25 @@ public class JavaClient {
 
                     break;
                 } else if (method.toString().equalsIgnoreCase("SEND")) {
+                    
+                    // Send a packet with our desired filename to be uploaded to
+                    // the server.
+                    sendPacket(fileName, sout);
+
                     System.out.println("Calling sendPacket() Method.");
-                    System.out.println("Sending file: 'Flight.docx'.");
+                    System.out.println("Sending file: " + fileName + "...");
 
                     try {
-                        while (!sock.isClosed()) {
-
-                            File myFile = new File(fileName);
-                            BufferedInputStream bis = new BufferedInputStream(new FileInputStream(myFile));
-                            OutputStream os = sock.getOutputStream();
-                            byte[] byteArray = new byte[BUFFER];
-                            while ((count = bis.read(byteArray)) >= 0) {
-                                os.write(byteArray, 0, count);
-                                os.flush();
-                            }
-                            sock.close();
+                        File myFile = new File(fileName);
+                        BufferedInputStream bis = new BufferedInputStream(new FileInputStream(myFile));
+                        OutputStream os = sock.getOutputStream();
+                        byte[] byteArray = new byte[BUFFER];
+                        while ((count = bis.read(byteArray)) > -1) {
+                            os.write(byteArray, 0, count);
+                            os.flush();
                         }
-                        System.out.println("File Transfer Finished.");
-
+                        System.out.println("File Transfer Finished: " + fileName + ".");
+                        os.close();
                     } catch (Exception e) {
                         e.printStackTrace();
                     }
--- JavaServer/src/JavaServer/JavaServer.java
+++ JavaServer/src/JavaServer/JavaServer.java
@@ -15,7 +15,7 @@ package JavaServer;
  * 
  * Usage of JavaServer:
  * 
- * java JavaServer
+ * java -jar "JavaServer.jar"
  * 
  * @author  David Tran      Java
  * @partner Martin Javier   Ruby
@@ -33,78 +33,106 @@ import java.net.Socket;
 
 public class JavaServer {
 
-    public JavaServer() {};
-    /**
-     * @param args the command line arguments
-     */
-    static private final int BUFFER = 2048;
-    static private Socket sock = null;
-    static private int count;
     
+    static private final int BUFFER = 8196;     // An int to specify buffer for transfer.
+    static private Socket sock = null;          // Globalized socket to be instantiated in main() method
+    static private int count;                   // A globalized int placeholder
+
+    /**
+     * Sets up the application to accept any incoming connections on the 
+     * specified port. Once connected, listen for the messages sent by the client.
+     * Return valid messages based on the protocols set up.
+     * 
+     * Once everything is verified and validated, conduct the file transfer.
+     */
     public static void main(String[] args) {
         try {
+            // As the server, we set up the ServerSocket class.
             ServerSocket servsock = new ServerSocket(7005);
             System.out.println("Listening for connections...");
             while (true) {
-              sock = servsock.accept();
-              System.out.println("Connected! Host: " + servsock.getInetAddress().getHostAddress()
-                      + " port: " + servsock.getLocalPort());
+                // accept all incoming requests on port 7005
+                sock = servsock.accept();
+                System.out.println("Connected! Host: " + servsock.getInetAddress().getHostAddress()
+                        + " port: " + servsock.getLocalPort());
 
-              InputStream in = sock.getInputStream();
-              
-              // Listen for GET or SEND packet,
-              // then call the appropriate method.
-              listen(in);
+                // Prep the input stream.
+                InputStream in = sock.getInputStream();
 
-              sock.close();
+                // Listen for GET or SEND packet,
+                // then call the appropriate method.
+                listen(in);
+
+                // Close the socket with the client after we're done.
+                sock.close();
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-    
-    static public void GET(String fileName) {
-        // send to client
+
+    /**
+     * GET() is an internal method that, with respect to the client, be sending 
+     * the requested file from the client. 
+     * 
+     * @param fileName - the fileName to be sent to the client.
+     */
+    static private void GET(String fileName) {
         System.out.println("GET() method called.");
         try {
             File myFile = new File(fileName);
             BufferedInputStream bis = new BufferedInputStream(new FileInputStream(myFile));
             OutputStream os = sock.getOutputStream();
             byte[] byteArray = new byte[BUFFER];
-            while((count = bis.read(byteArray)) >= 0) {
-                  os.write(byteArray, 0, count);
-                  os.flush();
-            } 
+            while ((count = bis.read(byteArray)) > -1) {
+                os.write(byteArray, 0, count);
+                os.flush();
+            }
             System.out.println("File Transfer Finished: " + fileName + ".");
             os.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
-    
-    static public void SEND(String fileName) {
-        // receive from client
+
+    /**
+     * SEND() is an internal method that, with respect to the client, be receiving
+     * the declared file from the client. 
+     * 
+     * @param fileName - the fileName to be received from the client.
+     */
+    static private void SEND(String fileName) {
         System.out.println("SEND() method called.");
+
+        byte[] buf = new byte[BUFFER];
+        
         try {
-            File myFile = new File(fileName);
             InputStream is = sock.getInputStream();
-            
-            FileOutputStream fos = new FileOutputStream(myFile);
+            FileOutputStream fos = new FileOutputStream(fileName);
             BufferedOutputStream bos = new BufferedOutputStream(fos);
-            
-            byte[] byteArray = new byte[BUFFER];
-            while ((count = is.read(byteArray)) >= 0) {
-                bos.write(byteArray, 0, count);
+
+            while ((count = is.read(buf)) > -1) {
+                bos.write(buf, 0, count);
             }
-            System.out.println("File Transfer finished: " + fileName + ".");
-            fos.close();
+            System.out.println("File Transfer finished.");
             bos.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
+
     }
-    
-    static public void sendPacket(String str, OutputStream sout) {
+
+    /**
+     * sendPacket() is an internal method that the application uses to send 
+     * messages to the Client based on the messages received from them.
+     * 
+     * The client has written protocols that will either accept or reject actions
+     * based on the validity of the message.
+     * 
+     * @param str - the Message to be sent to the client
+     * @param sout - the Output Stream
+     */
+    static private void sendPacket(String str, OutputStream sout) {
         byte[] bbuf = str.getBytes();
 
         try {
@@ -115,30 +143,47 @@ public class JavaServer {
             return;
         }
     }
-    
-    static public void listen(InputStream istr) {
+
+    /**
+     * listen() is an internal method that the application uses to listen from the
+     * Client. It has a a set of protocols that, upon receiving the expected message
+     * from the Client, either retrieves a file from the client or sends to the client
+     * the file requested.
+     * 
+     * @param istr - the Input Stream of the application listening from the Server
+     */
+    static private void listen(InputStream istr) {
         byte[] buf = new byte[BUFFER];
         int len;
         while (true) {
             try {
                 len = istr.read(buf, 0, BUFFER);
-            }
-            catch (IOException ioe) {
+            } catch (IOException ioe) {
                 System.err.println("Error with Packet transmission. Terminating connection.");
                 break;	// probably a socket ABORT; treat as a close
             }
-            
-            if (len == -1) break;
+
+            if (len == -1) {
+                break;
+            }
             String str = "";
             str = new String(buf, 0, len);
             System.out.println("Received: '" + str + "' from Host.");
 
+            // Our protocol.
+            // If str is GET or SEND, GET() or SEND() appropriately.
+            // Otherwise, break the loop and terminate the session.
             if (str.toString().equalsIgnoreCase("GET")) {
                 try {
                     sendPacket("ACK", sock.getOutputStream());
                     System.out.println("Calling GET() Method.");
+                    // Acknowledge and accept the file being requested.
+                    // Send it.
+                    String file = new String(buf, 0, istr.read(buf, 0, BUFFER));
+                    System.out.println(file);
+
                     Thread.sleep(2000);
-                    GET("text.txt");
+                    GET(file);
                 } catch (IOException ioe) {
                     System.err.println("ERROR: COULD NOT SEND PACKET!");
                     ioe.printStackTrace();
@@ -146,13 +191,15 @@ public class JavaServer {
                     e.printStackTrace();
                 }
                 break;
-                
+
             } else if (str.toString().equalsIgnoreCase("SEND")) {
                 try {
                     sendPacket("ACK", sock.getOutputStream());
                     System.out.println("Calling SEND() Method.");
-                    SEND("Flight.docx");
-                    Thread.sleep(2000);
+                    // Acknowledge the method. 
+                    String file = new String(buf, 0, istr.read(buf, 0, BUFFER));
+
+                    SEND(file);
                 } catch (IOException ioe) {
                     ioe.printStackTrace();
                 } catch (Exception e) {
@@ -166,13 +213,13 @@ public class JavaServer {
                 break;
             }
         }
-        
+
         try {
             istr.close();
         } catch (IOException ioe) {
             System.err.println("Error: Bad Stream Close");
             return;
         }
-            
+
     }
 }
